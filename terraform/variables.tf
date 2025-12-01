6bd208137fcc351dfb677598c3c23bde8170599a
anamsanchezm1 <am.sanchezm1@uniandes.edu.co>
2025-12-01 18:24:50 -0500

    Delete IAM role ARNs for ECS from variables.tf
    Removed variables for existing IAM roles for ECS.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -100,14 +100,3 @@ variable "cpu_target_value" {
   type        = number
   default     = 70
 }
-
-# 🔥 NUEVO: ARNs de los roles IAM EXISTENTES para ECS (NO se crean en Terraform)
-variable "ecs_task_execution_role_arn" {
-  description = "ARN of existing IAM role for ECS task execution"
-  type        = string
-}
-
-variable "ecs_task_role_arn" {
-  description = "ARN of existing IAM role for ECS tasks (application permissions)"
-  type        = string
-}
